fa32186996e0e30dce3b34cba776479e1d9c638a
Fazijutt762 <133943555+Fazijutt762@users.noreply.github.com>
2024-03-25 02:27:33 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -1,112 +1,48 @@
 import React from 'react';
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
-import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://vercel.hentai-movies-blog.com/graphql"
-	const graphQLClient = new GraphQLClient(endpoint);
-	const referringURL = ctx.req.headers?.referer || null;
-	const pathArr = ctx.query.postpath as Array<string>;
-	const path = pathArr.join('/');
-	console.log(path);
-	const fbclid = ctx.query.fbclid;
+    const referringURL = ctx.req.headers?.referer || null;
+    const fbclid = ctx.query.fbclid;
 
-	// redirect if facebook is the referer or request contains fbclid
-		if (referringURL?.includes('facebook.com') || fbclid) {
-
-		return {
-			redirect: {
-				permanent: false,
-				destination: `${
-					`https://vercel.hentai-movies-blog.com/` + encodeURI(path as string)
-				}`,
-			},
-		};
-		}
-	const query = gql`
-		{
-			post(id: "/${path}/", idType: URI) {
-				id
-				excerpt
-				title
-				link
-				dateGmt
-				modifiedGmt
-				content
-				author {
-					node {
-						name
-					}
-				}
-				featuredImage {
-					node {
-						sourceUrl
-						altText
-					}
-				}
-			}
-		}
-	`;
-
-	const data = await graphQLClient.request(query);
-	if (!data.post) {
-		return {
-			notFound: true,
-		};
-	}
-	return {
-		props: {
-			path,
-			post: data.post,
-			host: ctx.req.headers.host,
-		},
-	};
+    // Redirect if Facebook is the referrer or request contains fbclid
+    if (referringURL?.includes('facebook.com') || fbclid) {
+        return {
+            redirect: {
+                permanent: false,
+                destination: 'https://storater.pk', // Add your website URL here
+            },
+        };
+    }
+    
+    return {
+        props: {},
+    };
 };
 
 interface PostProps {
-	post: any;
-	host: string;
-	path: string;
+    imageUrl: string;
 }
 
-const Post: React.FC<PostProps> = (props) => {
-	const { post, host, path } = props;
-
-	// to remove tags from excerpt
-	const removeTags = (str: string) => {
-		if (str === null || str === '') return '';
-		else str = str.toString();
-		return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
-	};
-
-	return (
-		<>
-			<Head>
-				<meta property="og:title" content={post.title} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:type" content="article" />
-				<meta property="og:locale" content="en_US" />
-				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-				<meta
-					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
-				/>
-				<title>{post.title}</title>
-			</Head>
-			<div className="post-container">
-				<h1>{post.title}</h1>
-				<img
-					src={post.featuredImage.node.sourceUrl}
-					alt={post.featuredImage.node.altText || post.title}
-				/>
-				<article dangerouslySetInnerHTML={{ __html: post.content }} />
-			</div>
-		</>
-	);
+const Post: React.FC<PostProps> = ({ imageUrl }) => {
+    return (
+        <>
+            <Head>
+                <meta property="og:title" content="Title of Your Post" />
+                <meta property="og:description" content="Description of Your Post" />
+                <meta property="og:type" content="article" />
+                <meta property="og:locale" content="en_US" />
+                <meta property="og:site_name" content="Your Website Name" />
+                <meta property="og:image" content={imageUrl} /> {/* https://scontent.xx.fbcdn.net/v/t1.15752-9/432861005_300963956366525_3155662488465197906_n.png?_nc_cat=1&ccb=1-7&_nc_ohc=Qx4Lx4K39dEAX8drRQd&_nc_oc=AdjVqVkzFormG2QLtOSc-w3fgvNIzvTIvUhHJeww7HQqM0OOX1E3dWE-QhOCXoJZoV4_v93uL40S6uPTHGemx5rQ&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&stp=c0.5000x0.5000f_dst-jpg_flffffff_p500x261_q75&ur=567a6d&_nc_sid=7a31ef&oh=03_AdQsujeDmxf4XCPQoQu0IL5d8_ChzGaQ_8zEfbXcsYdo8g&oe=66275A64 */}
+                <meta property="og:image:alt" content="Image Alt Text" />
+                <title>Title of Your Post</title>
+            </Head>
+            <div className="post-container">
+                {/* Your post content */}
+            </div>
+        </>
+    );
 };
 
 export default Post;
